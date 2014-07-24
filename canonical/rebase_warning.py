15466ca294be714d6445423b52e3a4262e8927fb
aaronzhang231 <fenzhang@cisco.com>
2014-07-24 10:52:45 -0700

    added email functionality

--- canonical/rebase_warning.py
+++ canonical/rebase_warning.py
@@ -18,21 +18,21 @@ if __name__ == '__main__':
                         help = 'the file to store versions')
     args = parser.parse_args()
 
-    versions = {}
     changes = {}
     try:
         versions = pickle.load(open(args.filepath))
     except Exception:
+        versions = {}
         pass
     for comp in comps:
         try:
             output = subprocess.check_output(rmadison_cmd % {'comp': comp,
                                                              'ppa': args.ppa},
-                                             shell = True)
+                                             shell=True)
             # e.g. "neutron | 1:2014.1.1-0ubuntu2   | trusty-updates  | source"
             version, release = output.split('|')[1].split(':')[1].split('-')
-            if versions[comp] != version:
-                changes[comp] = {'old': versions[comp], 'new': version}
+            if versions.get(comp, None) != version:
+                changes[comp] = {'old': versions.get(comp, None), 'new': version}
                 versions[comp] = version
         except subprocess.CalledProcessError as e:
             # info for this comp and ppa doesn't exist
@@ -43,6 +43,8 @@ if __name__ == '__main__':
     if changes:
         # send email
         print 'change set: %s' % changes
+        subprocess.check_output('ssh chingwei@10.28.28.132 mail -s %s fenzhang@cisco.com cwchang@cisco.com' % changes,
+                                shell=True)
 
     with open(args.filepath, 'w') as f:
         pickle.dump(versions, f)
