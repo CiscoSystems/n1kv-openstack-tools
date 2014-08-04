4a76763ef4899edaccae292ebd10b52c5c56492a
ChingWei Chang <cwchang@cisco.com>
2014-08-03 20:40:51 -0700

    Made email mechanism work

--- canonical/rebase_warning.py
+++ canonical/rebase_warning.py
@@ -42,9 +42,8 @@ if __name__ == '__main__':
     if changes:
         # send email
         print 'change set: %s' % changes
-        subprocess.check_output(('ssh chingwei@10.28.29.132 mail -s "trusty rebase warning" ' 
-                                'fenzhang@cisco.com cwchang@cisco.com Cc: %s' % changes),
-                                shell=True)
+        subprocess.check_output(('ssh cwchang@10.28.29.132 "printf Need-rebase | mail -s \\"trusty rebase warning\\" cwchang@cisco.com"'), shell=True)
+
 
     with open(args.filepath, 'w') as f:
         pickle.dump(versions, f)
